ba2c5140ce84035e13a863ef21c7d573a943b7b3
Gioia Iacopini <gio.iacopini@gmail.com>
2018-01-03 13:39:02 +0100

    updates

--- boots_project/boots_project.py
+++ boots_project/boots_project.py
@@ -10,23 +10,42 @@ Created on Wed Nov 29 16:26:31 2017
 import random
 import matplotlib.pyplot
 import bootsframework
+import csv
 
 #create stores that are set in a location 
 
 stores=[]
 customers = []
 distances =[]
+revenue_list = []
+final_revenue_list=[]
+
 
 num_of_stores = 8
 num_of_customers = 50
-num_of_iterations= 500
+num_of_iterations= 100
 revenue = 0
 money = 100
 
+#read data: this file contains the revenues of the stores. 
+
+file = open("revenue.csv", "r")
+reader = csv.reader(file)
+
+#create a list of revenues 
+#the revenues are appended by default as strings. the following code turns the strings into integers
+for i in reader:
+    i = str(i)
+    i= i.strip("'[]'")
+    revenue_list.append(int(i))
+
+#We can print the revenue list to make sure that they are integers
+#print(revenue_list)
+
 #create stores
 for i in range(num_of_stores):
-    stores.append(bootsframework.Store((i+1), revenue))
-#assign stone sumbers (1-8) to stores)
+    stores.append(bootsframework.Store((i+1), revenue_list[i]))
+#assign store numbers (1-8) to stores)
 
 #generate customers 
 for i in range (num_of_customers):
@@ -38,12 +57,29 @@ for j in range(num_of_iterations):
     random.shuffle(customers)
     for i in range(num_of_customers):
         customers[i].move()
-        #customers[i].check_closest_store()
         customers[i].shop()
 
-#make function that will update the revenue of the stores in the store list using the revenues from the customers store list
-#write a file that will show the revenue of each store. 
-        
+#this for loop updates the revenue of the stores in the store list using the revenues from the customers store list        
+for i in range(num_of_stores):
+    for j in range(num_of_customers):
+        stores[i].revenue = (stores[i].revenue + customers[i].stores[i].revenue)
+
+#append the new revenues to the new list
+for store in stores:
+    final_revenue_list.append(stores[i].revenue)
+
+#print(final_revenue_list)
+               
+#write a file that will show the final revenue of each store. 
+finalrevenue= open("finalrevenue.csv", "a")
+writer = csv.writer(finalrevenue) 
+
+for i in final_revenue_list:
+    i = str(i)
+    writer.writerow(i)
+    
+finalrevenue.close
+       
 #plot the graph
 matplotlib.pyplot.ylim(0, 99)
 matplotlib.pyplot.xlim(0, 99)
@@ -53,3 +89,5 @@ for i in range (num_of_customers):
     matplotlib.pyplot.scatter(customers[i].x ,customers[i].y, color='tan')
 # colour the the customer closest to a shop a different colour
 matplotlib.pyplot.show()
+
+file.close
